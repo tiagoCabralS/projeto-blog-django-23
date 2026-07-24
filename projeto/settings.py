8974ab24076df8c41b62a9ac539c7536e970bf44
Tiago Cabral <tiagocabral.gja@gmail.com>
2026-07-24 16:43:21 -0300

    Update settings.py

--- projeto/settings.py
+++ projeto/settings.py
@@ -56,9 +56,7 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-                BASE_DIR / 'blog/templates'
-            ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
